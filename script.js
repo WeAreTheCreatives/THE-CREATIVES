ef82f23a45de13624395bcbfbc2737180ffa3406
WeAreTheCreatives <wearethecreativesng@gmail.com>
2025-05-04 20:40:43 +0100

    THIRD PROJECT

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-// Just a simple script to greet users in the console
-console.log("Welcome to my portfolio site! Feel free to explore.");
-
+// Attach click events to all gallery images
+document.addEventListener("DOMContentLoaded", () => {
+    const galleryImages = document.querySelectorAll(".gallery-img");
+    const modal = document.getElementById("imageModal");
+    const modalImg = document.getElementById("modalImage");
+  
+    galleryImages.forEach(img => {
+      img.addEventListener("click", () => {
+        modalImg.src = img.src;
+        modal.style.display = "block";
+      });
+    });
+  });
+  
+  function closeModal() {
+    document.getElementById("imageModal").style.display = "none";
+  }
+  
